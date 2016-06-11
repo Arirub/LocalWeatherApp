fb9e8ea5ff0e717c1b76959f0487a9ec4f2560c8
Ariane Rubio <arirub84@gmail.com>
2016-06-11 14:14:56 +0200

    Initial Commit

---
+++ LocalWeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalWeatherApp
+//
+//  Created by Ariane Rubio on 11/6/16.
+//  Copyright © 2016 Ariane Rubio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LocalWeatherApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LocalWeatherApp
+//
+//  Created by Ariane Rubio on 11/6/16.
+//  Copyright © 2016 Ariane Rubio. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
